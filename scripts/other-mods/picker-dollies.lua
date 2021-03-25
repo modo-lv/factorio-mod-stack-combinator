2722e294352571f2b8e86cef3a51f0d43a128982
Modo <modo@modo.lv>
2021-03-25 13:25:51 +0200

    Fixed (hopefully) PD multiplayer issue.

--- scripts/other-mods/picker-dollies.lua
+++ scripts/other-mods/picker-dollies.lua
@@ -14,42 +14,33 @@ end
 --- Wire up the event handler
 -- Has to be called from within another event otherwise remote.call doesn't work
 local function register()
-  if not (game.active_mods["PickerDollies"]) then
-    return
+  if (remote.interfaces["PickerDollies"]) then
+    events.register(remote.call("PickerDollies", "dolly_moved_entity_id"), moved,
+      function(ev)
+        return ev.moved_entity and ev.moved_entity.name == This.StaCo.NAME
+      end
+    )
+
+    events.register("dolly-rotate-rectangle", function(ev)
+      -- Figuring out which entity was being rotated would require accessing PD's game data etc., so
+      -- instead let's just ensure *all* StaCos are lined up with their outputs
+      for _, sc in pairs(This.runtime.combinators) do
+        moved({ moved_entity = sc.input })
+      end
+    end)
+
+    -- Log that we've registered
+    events.register(defines.events.on_player_joined_game, function()
+      Mod.logger:debug("Picker Dollies detected, registering move handlers.")
+    end)
   end
-
-  Mod.logger:debug("Picker Dollies detected, registering move handlers.")
-
-  events.register(remote.call("PickerDollies", "dolly_moved_entity_id"), moved,
-    function(ev)
-      return ev.moved_entity and ev.moved_entity.name == This.StaCo.NAME
-    end
-  )
-
-  events.register("dolly-rotate-rectangle", function(ev)
-    -- Figuring out which entity was being rotated would require accessing PD's game data etc., so
-    -- instead let's just ensure *all* StaCos are lined up with their outputs
-    for _, sc in pairs(This.runtime.combinators) do
-      moved({ moved_entity = sc.input })
-    end
-  end)
-end
-
---- Wire up the event handler on the first tick.
-local function register_tick()
-  register()
-  events.remove(defines.events.on_tick, register_tick)
 end
 
 ----------------------------------------------------------------------------------------------------
 
 function PickerDollies.register_all()
-  -- Unlike on_tick, this event also gets fired in map editor.
-  events.register(defines.events.on_player_joined_game, register)
-
-  -- Unlike on_player_joined_game, on_tick ensures that all mods have been added to
-  -- `game.active_mods` before firing
-  events.register(defines.events.on_tick, register_tick)
+  events.on_init(register)
+  events.on_load(register)
 end
 
 ----------------------------------------------------------------------------------------------------
