7f11f972fc3c759c69bfec913d94bc7a507e5f93
Modo <modo@modo.lv>
2021-03-27 18:31:25 +0200

    Added power requirement toggle.

--- scripts/settings-startup.lua
+++ scripts/settings-startup.lua
@@ -2,7 +2,8 @@
 --- Startup settings
 local StartupSettings = {
   -- Defaults
-  signal_capacity = { Mod.NAME .. "-signal-capacity", 40 }
+  signal_capacity = { Mod.NAME .. "-signal-capacity", 40 },
+  require_power = { Mod.NAME .. "-require-power", true }
 }
 
 ----------------------------------------------------------------------------------------------------
